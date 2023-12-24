41c664c2998af1cc0de37126129b193cecd4be5d
Sait N Yalcin <saitnyalcin@Saits-MacBook-Pro.local>
2023-12-24 12:28:07 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+const { execSync } = require("child_process");
+const path = require("path");
+const fs = require("fs");
+
+const generateFullStackApp = (appName) => {
+  const rootDir = path.join(process.cwd(), appName);
+  const frontendDir = path.join(rootDir, "frontend");
+  const backendDir = path.join(rootDir, "backend");
+
+  // Create root directory
+  fs.mkdirSync(rootDir);
+
+  // Create frontend and backend directories
+  fs.mkdirSync(frontendDir);
+  fs.mkdirSync(backendDir);
+
+  // Initialize frontend (React Vite)
+  execSync(`npm create vite@latest frontend -- --template react-ts`, { cwd: rootDir });
+
+  // Initialize backend (ASP.NET Core)
+  execSync(`dotnet new mvc -n backend`, { cwd: rootDir });
+
+  console.log("Full-stack application generated successfully!");
+};
+
+// Accept the app name as a command-line argument
+const appName = process.argv[2];
+
+if (!appName) {
+  console.error("Please provide a name for your full-stack app.");
+  process.exit(1);
+}
+
+generateFullStackApp(appName);
